3e8aff9645a6311ef748c8f5cdf79ae3629f4456
nazarhoza <nazarhoza@gmail.com>
2022-08-18 15:22:14 +0300

    new js

--- script.js
+++ script.js
@@ -4,51 +4,38 @@ const jacket = 123.965;
 const suit = 90.2345;
 
 
-console.log ("1.", pants); console.log ("2.", jacket); console.log ("3.", suit);
-
-
-console.log("4.Max=", Math.max(pants, jacket, suit));
-console.log("5.Min=", Math.min(pants, jacket, suit));
-
+document.writeln (`Максимальна ціна: <b> ${Math.max(pants, jacket, suit)}; </b>  <br>`);
+document.writeln(`Мінімальна ціна: <b>${Math.min(pants, jacket, suit)}; </b> <br>`);
 
 let sum = (pants + jacket + suit);
-console.log("6.Sum=", sum);
-
+document.writeln(`Вартість всіх товарів: <b>${sum}; </b> <br>`);
 
 console.log("7.", Math.floor(pants) ,"7.", Math.floor(jacket) , "7.", Math.floor(suit));
 
-
 let sumaFloor = Math.floor(pants) + Math.floor(jacket) + Math.floor(suit);
-console.log(sumaFloor);
-
+document.writeln(`Вартість всіх товарів без копійок: <b>${sumaFloor}; </b> <br>`);
 
 let  sumaRound = Math.round(sumaFloor / 100) * 100;
-console.log(sumaRound);
+document.writeln(`Вартість всіх товарів  до сотень: <b>${sumaRound}; </b> <br>`);
 
-
-console.log(sumaRound % 2 === 0) 
-
+document.writeln(`Вартість всіх товарів парне число: <b>${sumaRound % 2 === 0}; </b> <br>`);
 
 let restMoney = ( 500 - sum);
-console.log(restMoney);
-
+document.writeln(`Решта: <b>${restMoney}; </b> <br>`);
 
 let mediumCount = (pants + jacket + suit) / 3;
-console.log(mediumCount.toFixed(2))
-
+document.writeln(`Середнє значення: <b>${ mediumCount.toFixed(2)}; </b> <br>`);
 
 const MIN_VALUE = 1;
 const MAX_VALUE = 100;
 let randomSale = (MAX_VALUE - MIN_VALUE) * Math.random() + MIN_VALUE;
-console.log(randomSale.toFixed(0));
-
+document.writeln(`Рандомна знижка: <b>${randomSale.toFixed(0)}%; </b> <br>`);
 
 let resultBuy = (1 - (randomSale / 100)) * sum;
-console.log(resultBuy.toFixed(2));
-
+document.writeln(`Сума до оплати: <b>${resultBuy.toFixed(2)}; </b> <br>`);
 
 let lastChanse = (sum / 2) - (randomSale / 100 * sum);
-console.log(lastChanse);
+    document.writeln(`Чистий прибуток: <b>${lastChanse}; </b> <br>`);
 
  
 
