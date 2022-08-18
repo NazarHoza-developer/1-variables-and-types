17ee8c1275052cf8a89cd22e72721410d2388d0a
nazarhoza <nazarhoza@gmail.com>
2022-08-18 09:08:58 +0300

    new js

---
+++ index.js
@@ -0,0 +1,53 @@
+const a = 15.678;
+const b = 123.965;
+const c = 90.2345;
+
+
+console.log ("1.", a); console.log ("2.", b); console.log ("3.", c);
+
+
+console.log("4.Max=", Math.max(a, b, c));
+console.log("5.Min=", Math.min(a, b, c));
+
+
+let sum = (a + b + c);
+console.log("6.Sum=", sum);
+
+
+console.log("7.", Math.floor(a) ,"7.", Math.floor(b) , "7.", Math.floor(c));
+
+
+let sumaFloor = Math.floor(a) + Math.floor(b) + Math.floor(c);
+console.log(sumaFloor);
+
+
+let  sumaRound = Math.round(sumaFloor / 100) * 100;
+console.log(sumaRound);
+
+
+console.log(sumaRound % 2 === 0) 
+
+
+let restMoney = ( 500 - sum);
+console.log(restMoney);
+
+
+let mediumCount = (a + b + c) / 3;
+console.log(mediumCount.toFixed(2))
+
+
+const MIN_VALUE = 1;
+const MAX_VALUE = 100;
+let randomSale = (MAX_VALUE - MIN_VALUE) * Math.random() + MIN_VALUE;
+console.log(randomSale.toFixed(0));
+
+
+ let resultBuy = (1 - (randomSale / 100)) * sum;
+ console.log(resultBuy.toFixed(2));
+
+
+ let lastChanse = (sum / 2) - (randomSale / 100 * sum);
+ console.log(lastChanse);
+
+ 
+
